10e9ffcf8a1cc9fc65db38f65ee306e949c036fa
peter michael <enitanpeters28@gmail.com>
2022-09-20 11:21:06 +0100

    animation effect added

--- mycodes.js
+++ mycodes.js
@@ -22,3 +22,21 @@ mobile_item.forEach(item=> {
     mobile_menu.classList.toggle('active');
     })
 })
+
+function reveal() {
+ var reveals = document.querySelectorAll(".reveal");
+
+ for (var i = 0; i < reveals.length; i++) {
+  var windowHeight = window.innerHeight;
+  var elementTop = reveals[i].getBoundingClientRect().top;
+  var elementVisible = 200;
+
+  if (elementTop < windowHeight - elementVisible) {
+   reveals[i].classList.add("active");
+  } else {
+   reveals[i].classList.remove("active");
+  }
+ }
+}
+
+window.addEventListener("scroll", reveal);
